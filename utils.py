06ca66c6e17918e648181af3a61d4083d6a1b466
Swayam Agrahari <swayamagrahari025@gmail.com>
2025-10-04 00:45:48 +0530

    chore: remove monolith structure in preparation for modular backend/frontend split

--- utils.py
+++
@@ -1,9 +0,0 @@
-def getHeader():
-    agent = "Community Activity Alerts  (https://github.com/indictechcom/community-activity-alerts)"
-    url = "https://github.com/indictechcom/community-activity-alerts"
-    email = "tools.community-activity-alerts-system@toolforge.org"
-    return {
-        'User-Agent': agent,
-        'url': url,
-        'email': email
-    }
